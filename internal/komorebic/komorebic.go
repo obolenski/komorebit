1d5ea2b27bf3f8916091d198fe7658325996dfa8
obolenski <60326681+obolenski@users.noreply.github.com>
2024-09-24 19:20:34 +0200

    prevent console window flickering every time we call komorebic

--- internal/komorebic/komorebic.go
+++ internal/komorebic/komorebic.go
@@ -2,10 +2,12 @@ package komorebic
 
 import (
 	"os/exec"
+	"syscall"
 )
 
 func Exec(args []string) (string, error) {
 	cmd := exec.Command("komorebic.exe", args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	output, err := cmd.Output()
 	return string(output), err
 }
